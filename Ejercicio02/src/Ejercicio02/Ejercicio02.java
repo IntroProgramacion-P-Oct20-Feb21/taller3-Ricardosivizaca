8e52fdee12ad0c8cad60fff5742543b9454ed3de
Juan Ricardo Sivizaca Morocho <73245143+Ricardosivizaca@users.noreply.github.com>
2020-10-30 08:38:57 -0500

    Ejercicios taller3

---
+++ Ejercicio02/src/Ejercicio02/Ejercicio02.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio02;
+
+/**
+ *
+ * @author Elio Sivizaca
+ */
+public class Ejercicio02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double resultado = Math.pow(10,2)+10;
+        System.out.println(resultado);
+
+    }
+    
+}
